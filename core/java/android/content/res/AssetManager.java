59b437ee660e3a3dc98eec87bdd041220ebb24df
Sava Mikalacki <mikalackis@gmail.com>
2023-06-30 12:04:00 +0200

    ArielOS: Introduce Ariel platform

--- core/java/android/content/res/AssetManager.java
+++ core/java/android/content/res/AssetManager.java
@@ -82,6 +82,7 @@ public final class AssetManager implements AutoCloseable {
     private static final String FRAMEWORK_APK_PATH_DEVICE = "/system/framework/framework-res.apk";
     private static final String FRAMEWORK_APK_PATH_RAVENWOOD = "ravenwood-data/framework-res.apk";
     private static final String LINEAGE_APK_PATH = "/system/framework/org.lineageos.platform-res.apk";
+    private static final String ARIEL_APK_PATH = "/system/framework/com.arielos.platform-res.apk";
 
     private static final Object sSync = new Object();
 
@@ -290,6 +291,7 @@ public final class AssetManager implements AutoCloseable {
                 apkAssets.add(ApkAssets.loadOverlayFromPath(idmapPath, ApkAssets.PROPERTY_SYSTEM));
             }
             apkAssets.add(ApkAssets.loadFromPath(LINEAGE_APK_PATH, ApkAssets.PROPERTY_SYSTEM));
+            apkAssets.add(ApkAssets.loadFromPath(ARIEL_APK_PATH, ApkAssets.PROPERTY_SYSTEM));
 
             sSystemApkAssetsSet = new ArraySet<>(apkAssets);
             sSystemApkAssets = apkAssets.toArray(new ApkAssets[0]);
